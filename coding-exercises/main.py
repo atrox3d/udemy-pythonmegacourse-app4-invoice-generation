fac2762a02894b0cf29c0388f973a02fe20c4069
atrox3d <atrox3d@gmail.com>
2022-12-20 01:21:55 +0100

    Day 25: excercise: add comments

--- coding-exercises/main.py
+++ coding-exercises/main.py
@@ -3,15 +3,17 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
+# extract txt files from zip
 archivepath = "Text-Files.zip"
 destdir = "."
-
 with zipfile.ZipFile(archivepath, 'r') as archive:
     print(f"Extracting {archivepath}...")
     archive.extractall(destdir)
 
+# create pdf
 pdf = FPDF(orientation="P", unit="mm",  format="A4")
 for textfile in glob.glob("*.txt"):
+    # create one page for each txt file
     print(f"processing {textfile}...")
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
