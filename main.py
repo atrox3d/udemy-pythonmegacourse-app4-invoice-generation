47686382b372895a1b88957ca16328f1d9dcbeda
atrox3d <atrox3d@gmail.com>
2022-12-20 06:56:49 +0100

    Day 25: add a table

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ def pt2mm(points):
 
 for filepath in glob.glob("invoices/*.xlsx"):
     print(filepath)
-    df = pd.read_excel(filepath)
-    print(df)
-
     filename = Path(filepath).stem
     invoice, date = filename.split('-')
     print(invoice, date)
@@ -27,8 +24,33 @@ for filepath in glob.glob("invoices/*.xlsx"):
 
     pdf.set_font(family="Times", size=16, style="B")
     # pdf.write(h=8, txt=f"Date: {date}\n")
-    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
 
+    df = pd.read_excel(filepath)
+
+    # print headers dynamically
+    headers = [header.replace('_', ' ').title() for header in df.columns]
+    widths = [30, 70, 30, 30, 30]
+    for header, width in zip(headers, widths):
+        pdf.set_font(family="Times", size=10, style="B")
+        pdf.set_text_color(80, 80, 80)
+        print(header, width)
+        pdf.cell(w=width, h=8, border=1, txt=header)
+    pdf.ln()
+
+    # print rows
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, border=1, txt=str(row.product_id))
+        pdf.cell(w=70, h=8, border=1, txt=str(row.product_name))
+        pdf.cell(w=30, h=8, border=1, txt=str(row.amount_purchased))
+        pdf.cell(w=30, h=8, border=1, txt=str(row.amount_purchased))
+        pdf.cell(w=30, h=8, border=1, txt=str(row.total_price))
+        pdf.ln()
+
+
+    # create PDF
     outfile = Path(f"pdf/{filename}.pdf")
     outdir = outfile.parent
     if not outdir.exists():
